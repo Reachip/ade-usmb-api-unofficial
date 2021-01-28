aeec4a60610d2287b5d79e53d9319ebc82c00a8b
Rached Mejri <r.mejri74100@gmail.com>
2021-01-28 16:04:53 +0100

    feat: get ics folder by an env. variable

--- api/utils.py
+++ api/utils.py
@@ -5,13 +5,30 @@ from .timetable import TimeTable
 from .icalendar import ICalendar
 
 
+class ICSFolderPathNotProvided(Exception):
+    pass
+
+
+def get_ics_folder_path():
+    ics_folder = os.getenv("ADE_USMB_API_ICS_FOLDER")
+
+    if ics_folder is None:
+        raise ICSFolderPathNotProvided(
+            "You need to provide an ADE_USMB_API_ICS_FOLDER environnement variable which contain the path to the ICS files"
+        )
+
+    return ics_folder
+
+
 def sort_by(parameter, value, formatted_timetable):
     return [event for event in formatted_timetable if event[parameter] == value]
 
 
 def departement_names():
-    for departement_name in os.listdir("./ics"):
-        yield f"./ics/{departement_name}"
+    ics_folder_path = get_ics_folder_path()
+
+    for departement_name in os.listdir(ics_folder_path):
+        yield ics_folder_path + departement_name
 
 
 def init_formatters():
@@ -23,21 +40,21 @@ def init_formatters():
                 filename, extension = ics.split(".")
 
                 if extension == "ics":
+                    path = f"{departement_name}/{filename}.ics"
+
                     formatter = TimeTableFormatter(
-                        TimeTable(
-                            ICalendar.from_file(f"{departement_name}/{filename}.ics")
-                        )
+                        TimeTable(ICalendar.from_file(path))
                     ).format()
 
                     try:
                         formatter_per_departement[
-                            departement_name.split("/")[2]
+                            departement_name.split("/")[-1:][0]
                         ].append((filename, formatter))
 
                     except KeyError:
-                        formatter_per_departement[departement_name.split("/")[2]] = [
-                            (filename, formatter)
-                        ]
+                        formatter_per_departement[
+                            departement_name.split("/")[-1:][0]
+                        ] = [(filename, formatter)]
 
     return formatter_per_departement
 
